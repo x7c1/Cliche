26ba7932b5add787b4ac75d0d3aaa7f5b1cf30e7
x7c1 <jmtaro+x7c1@gmail.com>
2014-12-11 11:47:31 +0900

    delete unused build definition

--- scala/sample-taupe/project/TaupeBuild.scala
+++ scala/sample-taupe/project/TaupeBuild.scala
@@ -16,15 +16,8 @@ object TaupeBuild extends Build {
   )
   lazy val `taupe-app` = project.
     settings(taupeSettings:_*).
-    dependsOn(`taupe-lib`).
     dependsOn(ProjectRef(uri("git://github.com/x7c1/Salad.git#0.1"), "salad-lib"))
 
-  lazy val `taupe-lib` = project.
-    settings(taupeSettings:_*)
-
   lazy val root = Project("taupe", file(".")).
-    aggregate(
-      `taupe-app`,
-      `taupe-lib`
-    )
+    aggregate(`taupe-app`)
 }
