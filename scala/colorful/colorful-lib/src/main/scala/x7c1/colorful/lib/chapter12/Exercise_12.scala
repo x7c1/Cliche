ad0af8076d39347800eb06f2cd3609dc37ce9e63
x7c1 <jmtaro+x7c1@gmail.com>
2015-07-01 00:49:06 +0900

    codes for exercise 12.13

--- scala/colorful/colorful-lib/src/main/scala/x7c1/colorful/lib/chapter12/Exercise_12.scala
+++ scala/colorful/colorful-lib/src/main/scala/x7c1/colorful/lib/chapter12/Exercise_12.scala
@@ -201,3 +201,59 @@ trait Exercise_12_12[F[_]] {
     }
   }
 }
+trait Traverse[F[_]] {
+  def traverse[G[_]:Applicative,A,B](fa: F[A])(f: A => G[B]): G[F[B]] =
+    sequence(map(fa)(f))
+  def sequence[G[_]:Applicative,A](fga: F[G[A]]): G[F[A]] =
+    traverse(fga)(ga => ga)
+
+  def map[A,B](fa: F[A])(f: A => B): F[B]
+}
+
+case class Tree[+A](head: A, tail: List[Tree[A]])
+
+object Exercise_12_13 {
+  val listTraverse = new Traverse[List] {
+    override def traverse[G[_] : Applicative, A, B]
+      (fa: List[A])(f: A => G[B]): G[List[B]] = {
+
+      val g = implicitly[Applicative[G]]
+      val init: G[List[B]] = g.unit(List[B]())
+      fa.foldRight(init){(a, acc) => g.map2(f(a), acc){_ :: _} }
+    }
+
+    override def map[A, B](fa: List[A])(f: A => B): List[B] = fa.map(f)
+  }
+
+  val optionTraverse = new Traverse[Option] {
+    override def map[A, B](fa: Option[A])(f: A => B): Option[B] = fa map f
+
+    override def traverse[G[_] : Applicative, A, B]
+      (fa: Option[A])(f: A => G[B]): G[Option[B]] = {
+
+      val g = implicitly[Applicative[G]]
+      fa match {
+        case Some(a) => g.map(f(a))(Some(_))
+        case None => g unit None
+      }
+    }
+  }
+
+  val treeTraverse = new Traverse[Tree] {
+
+    override def traverse[G[_] : Applicative, A, B]
+      (fa: Tree[A])(f: A => G[B]): G[Tree[B]] = {
+
+      val tail: G[List[Tree[B]]] =
+        listTraverse.traverse(fa.tail){
+          ta => traverse(ta)(f)
+        }
+      val head: G[B] = f(fa.head)
+      implicitly[Applicative[G]].map2(head, tail){(h, t) => Tree(h, t)}
+    }
+
+    override def map[A, B](fa: Tree[A])(f: A => B): Tree[B] = {
+      Tree(f(fa.head), fa.tail.map(map(_)(f)))
+    }
+  }
+}
