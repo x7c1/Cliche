a19f73c1243ae0b382d877018b5e97667e4b5e28
haco <t.aida@japantaxi.co.jp>
2016-12-13 20:47:04 +0900

    add router sample

--- rust/rust-snow/src/main.rs
+++ rust/rust-snow/src/main.rs
@@ -1,12 +1,23 @@
 extern crate iron;
+extern crate router;
 
 use iron::prelude::*;
 use iron::status;
+use router::Router;
 
 fn main() {
-    fn hello_world(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "Hello!!")))
-    }
-    let _server = Iron::new(hello_world).http("localhost:3000").unwrap();
+    let mut router = Router::new();
+    router.get("/hello", hello_world, "hello");
+    router.get("/", index, "index");
+
+    let _server = Iron::new(router).http("localhost:3000").unwrap();
     println!("On 3000");
+}
+
+fn hello_world(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "Hello!!")))
+}
+
+fn index(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "index!!")))
 }
