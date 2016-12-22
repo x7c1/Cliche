cbbf32bc35a16955435944fa36c7bd1d93a280ec
x7c1 <jmtaro+x7c1@gmail.com>
2016-12-23 01:22:02 +0900

    support jar type by splicers factory

--- scala/android-khaki/project/CacheSplicersFactory.scala
+++ scala/android-khaki/project/CacheSplicersFactory.scala
@@ -2,20 +2,27 @@ import sbt.{File, ModuleID}
 
 class CacheSplicersFactory(cacheDirectory: File, unmanagedDirectory: File) {
 
+  def create(dependencies: Seq[String]): Seq[ArchiveCacheSplicer] = {
+    val create = new ArchiveCacheSplicer.Factory(cacheDirectory, unmanagedDirectory)
+    val caches = dependencies map ModuleIdFactory.create map toCache
+    filter(caches) map create.fromCache
+  }
+
   private val finder = new ArchiveCacheFinder(cacheDirectory)
 
-  private val factory = new ArchiveCacheSplicer.Factory(unmanagedDirectory)
-
-  def toCache(moduleID: ModuleID): ArchiveCache = {
+  private def toCache(moduleID: ModuleID): ArchiveCache = {
     finder search moduleID match {
-      case Left(error) =>
-        throw new IllegalArgumentException(error.message)
-      case Right(cache) =>
-        cache
+      case Left(error) => throw new IllegalArgumentException(error.message)
+      case Right(cache) => cache
     }
   }
 
-  def toSplicer(cache: ArchiveCache): ArchiveCacheSplicer = {
-    factory createFrom cache
+  private def filter(caches: Seq[ArchiveCache]): Seq[ArchiveCache] = {
+    val traverser = ArchiveCacheTraverser(cacheDirectory)
+    traverser traverse caches match {
+      case Left(error) => throw new IllegalArgumentException(error.message)
+      case Right(xs) => xs
+    }
   }
+
 }
