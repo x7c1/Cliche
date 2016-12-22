666ed26db958da406166a79af2e7735576ecc378
x7c1 <jmtaro+x7c1@gmail.com>
2016-12-23 01:22:59 +0900

    add ModuleIdFactory

---
+++ scala/android-khaki/project/ModuleIdFactory.scala
@@ -0,0 +1,17 @@
+import sbt.ModuleID
+
+object ModuleIdFactory {
+
+  /*
+    e.g.
+    target = "com.android.support:recyclerview-v7:25.0.1"
+   */
+  def create(target: String): ModuleID = {
+    val Array(groupId, artifactId, version) = target.split(':')
+    ModuleID(
+      organization = groupId,
+      name = artifactId,
+      revision = version
+    )
+  }
+}
